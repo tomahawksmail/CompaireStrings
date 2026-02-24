81b4f385df93f89cee667d8a93fd6ebcc8f6ad18
admin <H1abc7qlazx@>
2026-02-24 09:32:04 -0800

    add whois

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 docker build --no-cache -t string-compare-app:1 .
+docker stop comparison && docker rm comparison
 docker run -d --name comparison --restart unless-stopped -p 4999:4999 string-compare-app:1
