aa3f05ceac3af30060d8bd99aa2a1b749bfe189b
admin <H1abc7qlazx@>
2026-05-13 11:15:53 -0700

    add favicon

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-docker stop comparison && docker rm comparison
+docker stack rm comparison
 docker build --no-cache -t string-compare-app:1 .
-docker run -d --name comparison --restart unless-stopped -p 4999:4999 string-compare-app:1
+docker tag string-compare-app:1 dregistrygui.uskoinc.com/string-compare-app:1
+docker push dregistrygui.uskoinc.com/string-compare-app:1
+docker stack deploy -c service.yml comparison
